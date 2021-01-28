b98cea54bbbc4d969b9852ffdeb1f8e6ff8c7210
Ieva Dapševičiūtė <ieva.dapseviciute@telesoftas.com>
2021-01-28 14:36:24 +0200

    Add task execution code to main.swift

--- Sherlock and the Valid String/main.swift
+++ Sherlock and the Valid String/main.swift
@@ -7,5 +7,20 @@
 
 import Foundation
 
-print("Hello, World!")
+class Validator {
+    func isValid(_ string: String) -> Bool {
+        // Complete the isValid function
+        return true
+    }
+}
 
+let stdout = ProcessInfo.processInfo.environment["OUTPUT_PATH"]!
+FileManager.default.createFile(atPath: stdout, contents: nil, attributes: nil)
+let fileHandle = FileHandle(forWritingAtPath: stdout)!
+
+guard let s = readLine() else { fatalError("Bad input") }
+
+let result = Validator().isValid(s) ? "YES" : "NO"
+
+fileHandle.write(result.data(using: .utf8)!)
+fileHandle.write("\n".data(using: .utf8)!)
